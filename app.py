f7eefa34d15ee6f6312893cc17f3ed18b2eb79ae
Tumushimire Yves <yvestumushimire@gmail.com>
2020-08-15 15:16:36 +0200

    make it better

--- app.py
+++ app.py
@@ -12,12 +12,17 @@ class HelloWorld(object):
         bestaudio = video.getbestaudio(preftype="m4a")
         BESTFILE = os.getcwd() + "/media/" + str(title) + "." + str(bestaudio.extension)
         MP3FILE = os.getcwd() + "/media/" + str(title) + ".mp3"
+        COVER = os.getcwd() + "/media/cover.png"
+        MP3FINAL = os.getcwd() + "/media/[youtream.com]-" + str(title) + ".mp3"
         print (BESTFILE, MP3FILE)
         bestaudio.download(BESTFILE)
         print ("You have successfully downloaded the ."+str(bestaudio.extension)+" file")
-        command = "ffmpeg -i "+str(BESTFILE)+" -vn -ab 128k -ar 44100 -y "+str(MP3FILE)
-        subprocess.call(command, shell=True)
-        # os.remove(BESTFILE)
+        command1 = "ffmpeg -i "+str(BESTFILE)+" -vn -ab 128k -ar 44100 -metadata album=\"Youtream\" -y "+str(MP3FILE)
+        command2 = "ffmpeg -i "+str(MP3FILE)+" -i "+str(COVER)+" -map 0:0 -map 1:0 -c copy -id3v2_version 3 -metadata:s:v title='Album cover' -metadata:s:v comment='Cover (front)' -y "+str(MP3FINAL)
+        subprocess.call(command1, shell=True)
+        subprocess.call(command2, shell=True)
+        os.remove(BESTFILE)
+        os.remove(MP3FILE)
         return """
             <html>
             <head>
@@ -27,7 +32,7 @@ class HelloWorld(object):
             <a href="%(link)s" download>download</a>
             </body>
             </html>
-            """ %{ 'link': 'media'+ "/" + str(title) + ".mp3", 'file': str(title)}
+            """ %{ 'link': '/media/[youtream.com]-'+ str(title) + ".mp3", 'file': str(title)}
 
 config = {
     'global': {
